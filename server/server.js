e4c38b3b7f281ec888f67d95e0d92d17b1a1f4f7
William Truong <lordvinh1@gmail.com>
2018-04-18 23:53:59 -0500

    working on restful api

--- server/server.js
+++ server/server.js
@@ -14,12 +14,20 @@ app.post('/todos', (req, res) => {
     text: req.body.text
   });
 
+  console.log(req.body);
   todo.save().then((doc) => {
     res.send(doc);
   }, (e) => {
     res.status(400).send(e);
   });
-  console.log(req.body);
+});
+
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (e) => {
+    res.status(400).send(e);
+  });
 });
 
 app.listen(3000, () => {
